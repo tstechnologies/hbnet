1e311d86c65b290cce7a2b02c80544ecf29682ec
KF7EEL <kf7eel@qsl.net>
2022-03-23 08:27:04 -0700

    SMS format translation and web service configuration export, initial commit and untested

--- data_gateway.py
+++ data_gateway.py
@@ -1913,6 +1913,9 @@ def data_received(self, _peer_id, _rf_src, _dst_id, _seq, _slot, _call_type, _fr
                                 process_sms(_rf_src, msg_found, _call_type, UNIT_MAP[_rf_src][0])
                             if int_id(_dst_id) not in data_id:
                                 dashboard_sms_write(str(get_alias(int_id(_rf_src), subscriber_ids)), str(get_alias(int_id(_dst_id), subscriber_ids)), int_id(_dst_id), int_id(_rf_src), msg_found, time(), UNIT_MAP[_rf_src][0])
+                                if '$' in msg_found[0:1]:
+                                    send_sms(False, int_id(_dst_id), int_id(_rf_src), int_id(_rf_src), 'unit', msg_found)
+                                    
                             pass
 
                 # Reset assembly
