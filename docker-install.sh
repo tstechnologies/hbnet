316c3840de531b636a4b062e0cc98afd65d10624
TSTECH <95250581+tstechnologies@users.noreply.github.com>
2022-06-10 17:01:55 -0400

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 ## FOR USE/TESTED WITH DEBIAN 11 ##
+
 echo "Checking for Updates"
 sudo apt update
+
 echo "Applying Updates"
 sudo apt upgrade -y
+
 echo "Installing needed packages"
 sudo apt -y install apt-transport-https ca-certificates curl wget gnupg2 software-properties-common
+
 echo "Adding Repositories"
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/debian \
 $(lsb_release -cs) \
 stable"
-deb [arch=amd64] https://download.docker.com/linux/debian buster stable
+
 echo "Installing Docker and Docker-Compose"
 sudo apt update
 sudo apt install -y docker docker.io
@@ -25,4 +29,5 @@ sudo mv docker-compose-linux-x86_64 /usr/local/bin/docker-compose
 sudo curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 source /etc/bash_completion.d/docker-compose
 exit
+
 echo "Install Complete!"
